78dc70995d0115a82e0d230cd4b19bce21c261c9
azureaddeepak <94915453+azureaddeepak@users.noreply.github.com>
2022-10-11 00:06:44 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "rg_name" {
+  default = "RG-DEEP-001"
+}
+
+variable "rg_name2" {
+  default = "RG-MURALI-002"
+}
+
+variable "rglocation" {
+  default = "West Europe"
+}
+
+variable "storageacname2966" {
+  default = "uatopsstorageac45690"
+}
+
+variable "rg_name67" {
+  default = "RG-VENKAT-002"
+}
